d1902a3df7756ba16e1cee3bfa628d6372284fcc
vorrapard <78376065+vorrapard@users.noreply.github.com>
2023-10-05 15:35:23 -0400

    Update Contains Duplicate.py

--- Week1/Contains Duplicate.py
+++ Week1/Contains Duplicate.py
@@ -1,1 +1,18 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n <= 1:
+            return False
 
+        #Runtime
+        if len(set(nums)) != n:
+            return True
+        return False
+
+        #Memory Efficient
+        nums.sort()
+        print(nums)
+        for i in range(n):
+            if (nums[i-1] == nums[i]):
+                return True        
+        return False        
