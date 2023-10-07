3fafbbe2d6bf372c2ebfe8a681e57aee43c808bd
vorrapard <78376065+vorrapard@users.noreply.github.com>
2023-10-07 18:29:34 -0400

    Create Sliding Window Maximum.py

---
+++ Week1/Sliding Window Maximum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if len(nums) == 1:
+            return nums
+
+        output = []
+
+
+
+        return output
