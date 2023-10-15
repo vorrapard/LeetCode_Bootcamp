5d7c937e3eee40b468bf6901d24bdcf934f4d12d
vorrapard <78376065+vorrapard@users.noreply.github.com>
2023-10-14 22:07:26 -0400

    Update Minimum-window-substring.py

--- Week 2/Minimum-window-substring.py
+++ Week 2/Minimum-window-substring.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        n = len(s)
+        m = len(t)
+        if m>n or m==0:
+            return ""
+        
+        
+        
