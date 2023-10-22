bcb92b575959908d018d61d96b17ecdd57eb6bf8
vorrapard <78376065+vorrapard@users.noreply.github.com>
2023-10-22 13:41:11 -0400

    Update Merge k Sorted Lists.py

--- Week3/Merge k Sorted Lists.py
+++ Week3/Merge k Sorted Lists.py
@@ -5,4 +5,25 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        if len(lists) is 0:
+            return None
+        if len(lists) is 1:
+            return lists[0]
+
+        n = len(lists)
+        output_head = ListNode()
+        current = output_head
+        minval = lists[0].val
+        minindex = 0
+        
+        for i,item in enumerate(lists):
+            if item is None:
+                lists.pop(item)
+            if item.val < minval:
+                minval = item.val
+                minindex = i
+
+
+
         
