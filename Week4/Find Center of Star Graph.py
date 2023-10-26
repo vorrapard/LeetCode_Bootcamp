1d14f432d9142b4dab5f27f9783cc13ff2fff09c
vorrapard <78376065+vorrapard@users.noreply.github.com>
2023-10-25 21:16:54 -0400

    Update Find Center of Star Graph.py

--- Week4/Find Center of Star Graph.py
+++ Week4/Find Center of Star Graph.py
@@ -1,1 +1,18 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        n = len(edges)
 
+        count = [0] * (n+1)
+        for a,b in edges:
+            count[a-1] += 1
+            count[b-1] += 1
+
+        max_count = 0
+        for i in range(len(count)):
+            if count[i] > max_count:
+                max_count = count[i]
+                output = i+1
+        return output
+    
+
+        
