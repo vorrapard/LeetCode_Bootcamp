d69c8c5e9892cb1ffab0654ff1ff544831568a8a
vorrapard <78376065+vorrapard@users.noreply.github.com>
2023-10-29 18:22:23 -0400

    Update Minimum Add to Make Parentheses Valid.py

--- Week4/Minimum Add to Make Parentheses Valid.py
+++ Week4/Minimum Add to Make Parentheses Valid.py
@@ -1,1 +1,15 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack = []
+        count = 0
+        for char in s:
+            if char is "(":
+                stack.append(char)
+            elif char is ")":
+                if len(stack) == 0:
+                    count += 1
+                else:
+                    if stack[-1] is "(":
+                        stack.pop()
 
+        return len(stack) + count         
