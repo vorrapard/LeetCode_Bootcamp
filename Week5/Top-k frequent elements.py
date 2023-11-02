78c8b44a821601f8f9d1278e9c5ff9d40a0502aa
vorrapard <78376065+vorrapard@users.noreply.github.com>
2023-11-02 11:14:34 -0400

    Update Top-k frequent elements.py

--- Week5/Top-k frequent elements.py
+++ Week5/Top-k frequent elements.py
@@ -1,1 +1,15 @@
 
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        output = []
+        if n == 1:
+            return [nums[0]]
+
+        myDict = collections.Counter(nums)
+        index = sorted(myDict.values(), reverse=True)
+        for number in myDict:
+            if myDict[number] >= index[k-1]:
+                output.append(number)
+
+        return output     
